a604be4702b1efb9e638f7b0cc1b46ae8b8db0aa
amahdavid <amahdavid34@gmail.com>
2024-08-12 17:31:42 -0700

    Completed the question "Contains Duplicate"

--- Java/src/Array_Hashing/ContainsDuplicate.java
+++ Java/src/Array_Hashing/ContainsDuplicate.java
@@ -1,7 +1,45 @@
 package Array_Hashing;
 
+import java.util.HashMap;
+
+/**
+ * Question: Given an integer array nums, return true if any value appears more than once in the array
+ * otherwise return false.
+
+ * Examples: Input: nums = [1, 2, 3, 3], Output: true
+ * Input: nums = [1, 2, 3, 4], Output: false
+
+ * Solution:
+ * I will be making use of a hashmap,
+ * I will loop through the array of integers and store the integers as the Key and their frequencies as the value,
+ * I will then loop through the value of the hashmap and check if it contains 2 or more keys with more than one frequency
+ * */
+
 public class ContainsDuplicate {
+
+    public static boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Integer> output = new HashMap<>();
+        for (int num : nums) {
+            if (!output.containsKey(num)) {
+                output.put(num, output.getOrDefault(num, 1));
+            } else {
+                int freq = output.get(num) + 1;
+                output.put(num, freq);
+            }
+        }
+        for (int i : output.values()) {
+            if (i >= 2) {
+                return true;
+            }
+        }
+    return false;
+    }
     public static void main(String[] args) {
+        int[] example1 = {1, 2, 3, 3};
+        int[] example2 = { 1, 2, 3, 4};
+
+        System.out.println("Example 1 Output: " + hasDuplicate(example1));
+        System.out.println("Example 2 Output: " + hasDuplicate(example2));
 
     }
 }

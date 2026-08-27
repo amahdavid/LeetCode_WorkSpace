b3b9a3702e1f55bb98333f64d3e772809b7331a4
David Chika Amah-Nnachi <78812828+amahdavid@users.noreply.github.com>
2026-08-26 17:52:56 -0700

    Solve Valid Anagram in Python

---
+++ Python/ArraysAndHashing/ValidAnagram.py
@@ -0,0 +1,14 @@
+def isAnagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    count_s = {}
+    count_t = {}
+
+    for letter in s:
+        count_s[letter] = count_s.get(letter, 0) + 1
+
+    for letter in t:
+        count_t[letter] = count_t.get(letter, 0) + 1
+
+    return count_s == count_t

5dc61b1fb18cdefac4b3e50806e2757cac3a7b84
David Chika Amah-Nnachi <78812828+amahdavid@users.noreply.github.com>
2026-08-30 15:49:20 -0700

    Add ProductOfArrayExceptSelf.ts solution

---
+++ TypeScript/ArraysAndHashing/ProductOfArrayExceptSelf.ts
@@ -0,0 +1,18 @@
+function productExceptSelf(nums: number[]): number[] {
+    const result: number[] = new Array(nums.length).fill(1);
+    let runningProduct = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = runningProduct;
+        runningProduct *= nums[i];
+    }
+
+    runningProduct = 1;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= runningProduct;
+        runningProduct *= nums[i];
+    }
+
+    return result;
+}

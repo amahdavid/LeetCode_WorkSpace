18de86edcb5e3363ae6e941282ce4093c0a3d3ff
David Chika Amah-Nnachi <78812828+amahdavid@users.noreply.github.com>
2026-08-30 15:49:19 -0700

    Add ProductOfArrayExceptSelf.java solution

---
+++ Java/src/Array_Hashing/ProductOfArrayExceptSelf.java
@@ -0,0 +1,22 @@
+package Array_Hashing;
+
+public class ProductOfArrayExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int runningProduct = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = runningProduct;
+            runningProduct *= nums[i];
+        }
+
+        runningProduct = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= runningProduct;
+            runningProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
